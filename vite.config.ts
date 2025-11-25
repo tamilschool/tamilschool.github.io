bb7d7015f89a42c618e8b140be7ffb7324f74df5
Uttran Ishtalingam <dreamuth@gmail.com>
2025-11-25 13:59:45 -0600

    Use dev-root base and make manifest paths relative

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,13 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: '/kural/',
+  // Use root base during dev server so local paths work, and use '/kural/' for production build
+  base: command === 'serve' ? '/' : '/kural/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+}))
